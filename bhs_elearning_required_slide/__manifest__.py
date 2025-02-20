d6308e3896ed56bb908f2c8b00149f09f60915b0
Nguyen.Thi.Phuong.Dung <dungntp.bhsoft@gmail.com>
2025-02-20 14:45:51 +0700

    v17: update ava and banner

--- bhs_elearning_required_slide/__manifest__.py
+++ bhs_elearning_required_slide/__manifest__.py
@@ -18,7 +18,7 @@
     'author': 'Bac Ha Software',
     'website': 'https://bachasoftware.com',
     'maintainer': 'Bac Ha Software',
-    'images': ['static/description/banner.gif'],
+    'images': ['static/description/banner.png'],
     'installable': True,
     'auto_install': False,
     'assets': {
